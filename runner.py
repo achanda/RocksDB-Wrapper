b09e012bab8cd5b05fa13b77845f38e59e0732d9
Abhishek Chanda <abhishek.becs@gmail.com>
2025-02-27 03:52:36 +0000

    Update runner

--- runner.py
+++ runner.py
@@ -12,27 +12,28 @@ def main():
         "--size_ratio=4",
         "--entries_per_page=32",
         "--entry_size=128",
-        "--buffer_size_in_pages=16",
+        "--buffer_size_in_pages=128",
         "--stat=1"
     ]
-    
-    for bits in range(0, 17, 2):
-        filename = f"output_b{bits:02d}.txt"
-        command = base_command + [f"--bits_per_key={bits}"]
+   
+    for filter_type in [1,2]:
+        for bits in range(0, 17, 2):
+            filename = f"output_b{bits:02d}_{filter_type}.txt"
+            command = base_command + [f"--bits_per_key={bits}", f"--filter_type={filter_type}"]
         
-        print(f"Running with bits_per_key={bits}...", flush=True)
-        with open(filename, 'w') as f:
-            try:
-                subprocess.run(
+            print(f"Running with filter_type={filter_type} bits_per_key={bits}...", flush=True)
+            with open(filename, 'w') as f:
+                try:
+                    subprocess.run(
                     command,
                     stdout=f,
                     stderr=subprocess.STDOUT,
                     check=True
-                )
-            except subprocess.CalledProcessError as e:
-                print(f"Error occurred for bits_per_key={bits}: {e}", file=sys.stderr)
+                    )
+                except subprocess.CalledProcessError as e:
+                    print(f"Error occurred for bits_per_key={bits}: {e}", file=sys.stderr)
         
-        print(f"Completed bits_per_key={bits}", flush=True)
+            print(f"Completed bits_per_key={bits}", flush=True)
 
 if __name__ == "__main__":
     main()
