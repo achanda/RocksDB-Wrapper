b384f9e2c073109e297bb3f472b3cbad01708acd
Abhishek Chanda <abhishek.becs@gmail.com>
2025-03-17 02:04:00 +0000

    Update assets

--- runner3.py
+++ runner3.py
@@ -36,26 +36,5 @@ def main():
 
             print(f"Completed filter_type={filter_type} bits_per_key={bits} bloom_before_level={bloom_before_level}", flush=True)
     
-    '''
-    for filter_type in [1,2]:
-        for bits in range(0, 17, 2):
-            filename = f"output_b{bits:02d}_{filter_type}.txt"
-            command = base_command + [f"--bits_per_key={bits}", f"--filter_type={filter_type}"]
-        
-            print(f"Running with filter_type={filter_type} bits_per_key={bits}...", flush=True)
-            with open(filename, 'w') as f:
-                try:
-                    subprocess.run(
-                    command,
-                    stdout=f,
-                    stderr=subprocess.STDOUT,
-                    check=True
-                    )
-                except subprocess.CalledProcessError as e:
-                    print(f"Error occurred for bits_per_key={bits}: {e}", file=sys.stderr)
-        
-            print(f"Completed bits_per_key={bits}", flush=True)
-    '''
-
 if __name__ == "__main__":
     main()
